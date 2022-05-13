73f6c2de45791893af2f396e7e4fdf5ceebaf6b2
Sigrid Keydana <keydana@gmx.de>
2022-05-13 10:02:42 +0200

    use individual optimizer's defaults

--- R/utils-testopt.R
+++ R/utils-testopt.R
@@ -131,8 +131,8 @@ domain_sphere <- function(){
 #'
 #' @param optim          Torch optimizer function.
 #' @param ...            Additional parameters (passed to `image` function).
-#' @param opt_hparams    A list with optimizer initialize parameters
-#'   (default `list(lr = 0.01)`).
+#' @param opt_hparams    A list with optimizer initialization parameters (default: NULL).
+#' If missing, for each optimizer its individual defaults will be used.
 #' @param test_fn        A test function (default `"beale"`). You can also pass
 #'   a list with 2 elements. The first should be a function that will be optimized
 #'   and the second is a function that returns a named vector with `x0`, `y0`
@@ -155,7 +155,7 @@ domain_sphere <- function(){
 #'
 #' @export
 test_optim <- function(optim, ...,
-                       opt_hparams = list(lr = 0.01),
+                       opt_hparams = NULL,
                        test_fn = "beale",
                        steps = 200,
                        pt_start_color = "#5050FF7F",
@@ -210,7 +210,10 @@ test_optim <- function(optim, ...,
     y <- torch::torch_tensor(y0, requires_grad = TRUE)
 
     # instantiate optimizer
-    optim <- do.call(optim, c(list(params = list(x, y)), opt_hparams))
+    if(!missing(opt_hparams))
+        optim <- do.call(optim, c(list(params = list(x, y)), opt_hparams))
+    else
+        optim <- do.call(optim, c(list(params = list(x, y))))
     grad_keep <-  FALSE
     if (!is.null(optim$classname) && optim$classname == c("optim_adahessian")) {
         grad_keep <- TRUE
